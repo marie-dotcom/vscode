d3911471ba9bc040e7834461bb3658dae02c2e91
marie-dotcom <annemarieomundi@gmail.com>
2023-11-12 13:30:20 +0300

    added a search city

--- src/index.js
+++ src/index.js
@@ -103,4 +103,5 @@ function formatDay(timestamp) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
+searchCity("Nairobi")
 
